c9c07bcf9038ea35b2746a4f22927968bec3eb82
Alex Vega <alexvega@Alexs-MacBook-Pro.local>
2023-06-21 18:53:15 -0400

    this adds the prompts to receive user input

--- index.js
+++ index.js
@@ -4,8 +4,24 @@ const inquirer = require ('inquirer');
 inquirer
     .prompt([
         {
-        type: 'input',
-        name: 'text',
-        message: "Enter the characters of your logo",
+            type: 'input',
+            name: 'text',
+            message: "Enter the characters of your logo",
         },
-    ])
+        {
+            type: 'input',
+            name: 'textColor',
+            message: "Input text color - enter color keyword or a hexadecimal number",
+        },
+        {
+            type: 'list',
+            name: 'logoShape',
+            message: "Select the shape of your logo",
+            choices: ['Square', 'Circle', 'Triangle'],
+        },
+        {
+            type: 'input',
+            name: 'shapeColor',
+            message: "Input shape color - enter color keyword or a hexadecimal number",
+        },
+    ]);
