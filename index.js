032c4c31e76f78cdd62a712ca6e4de8a126ccd99
Alex Vega <alexvega@Alexs-MacBook-Pro.local>
2023-06-22 18:52:07 -0400

    adjust folder naming

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require ('fs');
 const inquirer = require ('inquirer');
-const logoCreator = require ('./utils/logoCreator');
+const logoCreator = require ('./lib/logoCreator');
 
 inquirer
     .prompt([
